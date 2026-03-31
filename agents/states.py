52221dc151d0d1f470c6d08a963d2309db3e3dcc
nandhu-ramesh-17 <nandhu_ramesh@reizend.ai>
2026-03-31 11:38:34 +0530

    Added states for Architect

--- agents/states.py
+++ agents/states.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 class File(BaseModel):
     path : str = Field(description="Path to file to be created or modified")
@@ -10,3 +10,11 @@ class Plan(BaseModel):
     techstack: str = Field(description="List of technologies to be used in the project. eg : 'Python', 'FastAPI', 'PostgreSQL', 'Docker' etc.")
     features : list[str] = Field(description="List of features to be implemented in the app. eg : 'User authentication', 'Data visualization dashboard', 'REST API endpoints' etc.")        
     files : list[File] = Field(description="List of files to be created or modified to complete the project. Each with path and purpose.")
+
+class ImplementationTask(BaseModel):
+    filepath: str = Field(description="The path to the file to be modified")
+    task_description: str = Field(description="A detailed description of the task to be performed on the file, e.g. 'add user authentication', 'implement data processing logic', etc.")
+
+class TaskPlan(BaseModel):
+    implementation_steps: list[ImplementationTask] = Field(description="A list of steps to be taken to implement the task")
+    model_config = ConfigDict(extra="allow")
