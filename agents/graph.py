d0a43b01a9f4dc992b5b7ac1aaf373f81dcda072
nandhu-ramesh-17 <nandhu_ramesh@reizend.ai>
2026-03-31 11:05:43 +0530

    Added the base Graph - Planner Node

--- agents/graph.py
+++ agents/graph.py
@@ -1,6 +1,9 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from langgraph.graph import StateGraph
+from langgraph.constants import END
+
 from states import *
 from prompts import *
 
@@ -8,10 +11,19 @@ load_dotenv()
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
+def planner_agent(state : dict) -> dict:
+    user_prompt = state["user_prompt"]
+    response = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    return {"plan" : response}
+
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
+graph.set_entry_point("planner")
+
+agent = graph.compile()
+
 user_prompt = "Build a Simple Calculator web app"
 
-prompt = planner_prompt(user_prompt)
+result = agent.invoke({"user_prompt" : user_prompt})
 
-response = llm.with_structured_output(Plan).invoke(prompt)
-
-print(response)
+print(result)
